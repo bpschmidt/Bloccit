cdf40b0536e53f0fb5117850a3469b6c35a91ccf
Brenda Schmidt <musicchick5285@yahoo.com>
2015-11-02 16:22:17 -0500

    Assignment 33 in progress

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: "Boolean") }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: true) }
   describe "attributes" do
     it "should respond to title" do
       expect(question).to respond_to(:title)
