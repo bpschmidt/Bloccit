6c2d9001c06051041d675f38dbcc66291f55f17d
Brenda Schmidt <musicchick5285@yahoo.com>
2015-11-15 22:09:57 -0500

    Progress on assignment 40

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,6 +38,10 @@ RSpec.describe User, type: :model do
     it "should respond to member?" do
       expect(user).to respond_to(:member?)
     end
+
+    it "should respond to moderator?" do
+      expect(user).to respond_to(:moderator?)
+    end
   end
 
   describe "roles" do
@@ -67,6 +71,20 @@ RSpec.describe User, type: :model do
 
       it "should return true for #admin?" do
         expect(user.admin?).to be_truthy
+      end
+    end
+
+    context "moderator user" do
+      it "should return true for #moderator?" do
+        expect(user.moderator?).to be_truthy
+      end
+
+      it "should return false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "should return false for #admin?" do
+        expect(user.admin?).to be_falsey
       end
     end
   end
