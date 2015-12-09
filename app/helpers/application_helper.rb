3e467f66ce9412012ee34a25d48870c7a60d46b6
Brenda Schmidt <musicchick5285@yahoo.com>
2015-12-09 17:30:59 -0500

    Assignment 45 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+    def posts_list(zebra)
+      if zebra.posts.any?
+        render zebra.posts
+      else
+        "No Posts"
+      end
+    end
+
+
     def form_group_tag(errors, &block)
       if errors.any?
         content_tag :div, capture(&block), class: 'form-group has-error'
