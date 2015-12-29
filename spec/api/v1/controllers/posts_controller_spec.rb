2513f181090fe6535750fc39b2f6ca65aae4720b
Brenda Schmidt <musicchick5285@yahoo.com>
2015-12-28 23:50:38 -0500

    Working on API posts controller spec

--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -7,16 +7,31 @@ RSpec.describe Api::V1::PostsController, type: :controller do
   let (:my_topic) { create(:topic) }
   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
 
-  context "authorized user" do
-    before do
-      controller.request.env['HTTP_AUTHORIZATION'] =  ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
-      controller.authenticate_user
+  context "unauthenticated user" do
+    it "GET index returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
     end
 
-    describe "authenticated user" do
-      it "finds a user by their authentication token" do
-        expect(assigns(:current_user)).to eq my_user
-      end
+    it "GET show returns http success" do
+      get :show, id: my_post.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context "unauthorized user" do
+    before do
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_post.auth_token)
+    end
+
+    it "GET index returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "GET show returns http success" do
+      get :show, id: my_post.id
+      expect(response).to have_http_status(:success)
     end
   end
 end
