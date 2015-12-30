74cce9fd438d296b83774893f5f04c4aedc4116f
Brenda Schmidt <musicchick5285@yahoo.com>
2015-12-29 23:57:31 -0500

    Assignment progress- altered routes, posts/comments controller specs

--- spec/api/v1/controllers/comments_controller_spec.rb
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 include RandomData
 
-RSpec.describe Api::V1::PostsController, type: :controller do
+RSpec.describe Api::V1::CommentsController, type: :controller do
   let(:my_user) { create(:user) }
   let(:other_user) { create(:user) }
   let (:my_topic) { create(:topic) }
   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
   let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
-
+  
   context "unauthenticated user" do
     it "GET index returns http success" do
       get :index
@@ -15,7 +15,7 @@ RSpec.describe Api::V1::PostsController, type: :controller do
     end
 
     it "GET show returns http success" do
-      get :show, id: my_post.id
+      get :show, id: my_comment.id
       expect(response).to have_http_status(:success)
     end
   end
@@ -31,7 +31,7 @@ RSpec.describe Api::V1::PostsController, type: :controller do
     end
 
     it "GET show returns http success" do
-      get :show, id: my_post.id
+      get :show, id: my_comment.id
       expect(response).to have_http_status(:success)
     end
   end
